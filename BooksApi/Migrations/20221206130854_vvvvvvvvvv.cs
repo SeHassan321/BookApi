f914a567ab77cd198ac196e80c41a1b2cc6b3790
Hassan <se.hassan321@gmail.com>
2022-12-06 15:11:11 +0200

    Migration rebuild

---
+++ BooksApi/Migrations/20221206130854_vvvvvvvvvv.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksApi.Migrations
+{
+    public partial class vvvvvvvvvv : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_FavoritLists_FavoritListId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FavoritListId",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_FavoritLists_FavoritListId",
+                table: "Books",
+                column: "FavoritListId",
+                principalTable: "FavoritLists",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_FavoritLists_FavoritListId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FavoritListId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_FavoritLists_FavoritListId",
+                table: "Books",
+                column: "FavoritListId",
+                principalTable: "FavoritLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
